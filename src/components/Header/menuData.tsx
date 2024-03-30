d417c75a3ce86c3b8f909a005cc5210753362812
Deepanshu <dr.d33panshuraj@gmail.com>
2024-03-30 11:00:41 +0530

    initial commit

---
+++ src/components/Header/menuData.tsx
@@ -0,0 +1,24 @@
+import { Menu } from "@/types/menu";
+
+const menuData: Menu[] = [
+  {
+    id: 1,
+    title: "Home",
+    path: "/",
+    newTab: false,
+  },
+  {
+    id: 2,
+    title: "About",
+    path: "/about",
+    newTab: false,
+  },
+  {
+    id: 33,
+    title: "Blog",
+    path: "/blog",
+    newTab: false,
+  },
+ 
+];
+export default menuData;
